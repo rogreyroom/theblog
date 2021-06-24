d2742fb5c2e760a3c41fc0297ab534449c2bf6c4
Robert Adamczewski <robert@adamczewski.me>
2021-06-24 17:53:14 +0200

    feat: create basic react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.scss';
+
+export const App = () => {
+  return (
+    <>
+      <h1>Hello to THE BLOG!!!</h1>
+    </>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));

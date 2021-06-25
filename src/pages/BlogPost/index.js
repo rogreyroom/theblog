f95969bbc77dc4e03f3cfe7793e3ca4dbcbdb32e
Robert Adamczewski <robert@adamczewski.me>
2021-06-25 21:09:58 +0200

    feat: get user for the post and display it

--- src/pages/BlogPost/index.js
+++ src/pages/BlogPost/index.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { commentsActions } from '../../redux/comments/commentsSlice'
+import { usersActions } from '../../redux/users/usersSlice'
 import CommentsList from '../../components/CommentsList'
 
 const BlogPost = () => {
   const { post } = useSelector((state) => state.posts)
+  const { user } = useSelector((state) => state.users)
   const dispatch = useDispatch()
 
   useEffect(() => {
     if ({}.hasOwnProperty.call(post, 'id')) {
       dispatch(commentsActions.getPostComments(post.id))
+      dispatch(usersActions.getUserById(post.userId))
     }
   }, [dispatch, post])
 
@@ -17,6 +20,7 @@ const BlogPost = () => {
 
   return (
     <>
+      <span>{user.name}</span>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
       <div>

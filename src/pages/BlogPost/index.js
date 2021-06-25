5ee34d5f1930347c029431dce9c75f3c63099c2a
Robert Adamczewski <robert@adamczewski.me>
2021-06-25 17:49:51 +0200

    feat: get comments for a single post and display as a list

--- src/pages/BlogPost/index.js
+++ src/pages/BlogPost/index.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { commentsActions } from '../../redux/comments/commentsSlice'
+import CommentsList from '../../components/CommentsList'
 
 const BlogPost = () => {
   const { post } = useSelector((state) => state.posts)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if ({}.hasOwnProperty.call(post, 'id')) {
+      dispatch(commentsActions.getPostComments(post.id))
+    }
+  }, [dispatch, post])
 
   if (!{}.hasOwnProperty.call(post, 'id')) return <h2>Loading...</h2>
 
@@ -10,6 +19,9 @@ const BlogPost = () => {
     <>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
+      <div>
+        <CommentsList />
+      </div>
     </>
   )
 }

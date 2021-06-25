90bb2b5a9b4d4356ff5fbe7d480d3d5d87a2580d
Robert Adamczewski <robert@adamczewski.me>
2021-06-25 17:35:35 +0200

    feat: fetch comments to the store when posts are ready

--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-
+import { useSelector, useDispatch } from 'react-redux'
 import { postsActions } from '../../redux/posts/postsSlice'
+import { fetchCommentsData } from '../../redux/comments/commentsActions'
 
 const BlogList = () => {
   const { posts } = useSelector((state) => state.posts)
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (posts.length > 0) {
+      dispatch(fetchCommentsData())
+    }
+  }, [dispatch, posts])
 
   const setPostData = (id) => {
     dispatch(postsActions.getPostById(id))

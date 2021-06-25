457e19d51492e4edb2fc7bb2764e4e96c8b9e518
Robert Adamczewski <robert@adamczewski.me>
2021-06-25 21:06:43 +0200

    feat: create redux store for users

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postsSlice from './posts/postsSlice'
 import commentsSlice from './comments/commentsSlice'
+import usersSlice from './users/usersSlice'
 
 const store = configureStore({
   reducer: {
     posts: postsSlice.reducer,
     comments: commentsSlice.reducer,
+    users: usersSlice.reducer,
   },
 })
 

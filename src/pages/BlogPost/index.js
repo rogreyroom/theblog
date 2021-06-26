f1feaa99f1e723f55d72f703b422227fea30f3e2
Robert Adamczewski <robert@adamczewski.me>
2021-06-26 08:06:38 +0200

    feat: get photo for the post and display it

--- src/pages/BlogPost/index.js
+++ src/pages/BlogPost/index.js
@@ -3,16 +3,19 @@ import { useSelector, useDispatch } from 'react-redux'
 import { commentsActions } from '../../redux/comments/commentsSlice'
 import { usersActions } from '../../redux/users/usersSlice'
 import CommentsList from '../../components/CommentsList'
+import { photosActions } from '../../redux/photos/photosSlice'
 
 const BlogPost = () => {
   const { post } = useSelector((state) => state.posts)
   const { user } = useSelector((state) => state.users)
+  const { photo } = useSelector((state) => state.photos)
   const dispatch = useDispatch()
 
   useEffect(() => {
     if ({}.hasOwnProperty.call(post, 'id')) {
       dispatch(commentsActions.getPostComments(post.id))
       dispatch(usersActions.getUserById(post.userId))
+      dispatch(photosActions.getPhotoById(post.id))
     }
   }, [dispatch, post])
 
@@ -20,6 +23,7 @@ const BlogPost = () => {
 
   return (
     <>
+      <img src={photo.url} alt={photo.title} />
       <span>{user.name}</span>
       <h2>{post.title}</h2>
       <p>{post.body}</p>

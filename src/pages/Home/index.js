a0967df4b7a6cce92fe1d1e3f227c88f98ffbce7
Robert Adamczewski <robert@adamczewski.me>
2021-06-26 08:05:42 +0200

    feat: fetch photos to the redux store

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchPostsData } from '../../redux/posts/postsActions'
 import BlogList from '../../components/BlogList'
+import { fetchPhotosData } from '../../redux/photos/photosActions'
 
 const Home = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(fetchPostsData())
+    dispatch(fetchPhotosData())
   }, [dispatch])
 
   return (

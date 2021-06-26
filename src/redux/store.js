57632ef41b44e00bfa0b4494aecc90a34fe7e519
Robert Adamczewski <robert@adamczewski.me>
2021-06-26 08:04:09 +0200

    feat: create redux store for photos

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import postsSlice from './posts/postsSlice'
 import commentsSlice from './comments/commentsSlice'
 import usersSlice from './users/usersSlice'
+import photosSlice from './photos/photosSlice'
 
 const store = configureStore({
   reducer: {
     posts: postsSlice.reducer,
     comments: commentsSlice.reducer,
     users: usersSlice.reducer,
+    photos: photosSlice.reducer,
   },
 })
 

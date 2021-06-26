ebd00f8e7f98d1ba16bace054ed115168689a117
Robert Adamczewski <robert@adamczewski.me>
2021-06-26 10:05:53 +0200

    feat: add normalize.css to the project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import NavBar from './components/NavBar'
 import Routes from './Router'
+import 'normalize.css'
 import './App.scss'
 
 export const App = () => (

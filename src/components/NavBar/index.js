321730124d0caf9aa7d5b68c6a47ac23025a5bcd
Robert Adamczewski <robert@adamczewski.me>
2021-06-26 17:08:18 +0200

    feat: create styles and add them to the NavBar component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import classes from './index.module.scss'
 
 const NavBar = () => (
-  <header>
-    <nav>
-      <Link to="/">
-        <h1>Hello to THE BLOG!!!</h1>
+  <header className={classes.header}>
+    <nav className={classes.nav}>
+      <Link to="/" className={classes.nav__link}>
+        <p className={classes.nav__text}>home</p>
       </Link>
     </nav>
+    <Link to="/" className={classes.header__link}>
+      <h1 className={classes.header__logo}>TheBlog</h1>
+    </Link>
   </header>
 )
 
